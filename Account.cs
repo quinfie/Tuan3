e6b94110561947eba304edce6ac12f277db6656b
quinfie <142389819+quinfie@users.noreply.github.com>
2023-08-30 16:22:27 +0700

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tuan3
+{
+    class Account
+    {
+        private long accountNumber;
+        private string name;
+        private double balance;
+        private double RATE = 0.035;
+
+        public long AccountNumber
+        {
+            get { return accountNumber; }
+            set { accountNumber = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+    }
+}
