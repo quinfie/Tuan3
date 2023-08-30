e2eca8a51cdd5b01f911d6de4fcbe218cf2d5ccc
A209PC13\Administrator <tn160803@gmail.com>
2023-08-30 14:24:40 +0700

    first commit

--- tuan3/HangThucPham.cs
+++ tuan3/HangThucPham.cs
@@ -8,5 +8,47 @@ namespace tuan3
 {
     class HangThucPham
     {
+        private int maHang;
+        private string tenHang;
+        private int donGia;
+        private DateTime ngaySX;
+        private DateTime ngayHH;
+
+        public int MaHang
+        {
+            get { return maHang; }
+            set
+            {
+                maHang = value;
+                if (maHang < 0)
+                    throw new Exception("Khong hop le");
+            
+            }
+        }
+        public string TenHang
+        {
+            get { return tenHang; }
+            set
+            {
+                tenHang = value;
+                tenHang = "";
+            }
+        }
+        public int DonGia
+        {
+            get { return donGia; }
+            set
+            {
+                donGia = value;
+                if (donGia <= 0)
+                    throw new Exception("Khong hop le");
+
+            }
+        }
+        public DateTime NgaySX
+        {
+
+        }
     }
+
 }
